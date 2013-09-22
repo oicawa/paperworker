093adf7a849e59375038cf14938aa693425ccdee
Masamitsu Oikawa <oicawa@gmail.com>
2013-09-22 13:54:42 +0900

    Added a project for Holiday Division.

---
+++ paperworker.holidaydivision.core/src/paperworker/holidaydivision/core/HolidayDivision.java
@@ -0,0 +1,98 @@
+/*
+ *  $Id: HolidayDivision.java 2013/09/21 3:03:36 Masamitsu Oikawa $
+ *
+ *  ===============================================================================
+ *
+ *   Copyright (C) 2013  Masamitsu Oikawa  <oicawa@gmail.com>
+ *   
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *   
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ *
+ *  ===============================================================================
+ */
+
+package paperworker.holidaydivision.core;
+
+import paperworker.core.PWItem;
+import paperworker.core.PWError;
+import paperworker.core.annotation.PWFieldBasicInfo;
+import paperworker.core.annotation.PWItemBasicInfo;
+import paperworker.master.core.MasterItem;
+
+@PWItemBasicInfo(caption = "Holiday Divisions", tableName = "HolidayDivisions")
+public class HolidayDivision extends MasterItem {
+	
+	public String getDivisionId() {
+		return divisionId;
+	}
+
+	public void setDivisionId(String divisionId) {
+		this.divisionId = divisionId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	@PWFieldBasicInfo(caption = "Holiday Division ID", type = "varchar(100)", primary = true)
+	private String divisionId;
+
+	@PWFieldBasicInfo(caption = "Holiday Division Name", type = "varchar(100)")
+	private String name;
+	
+	@PWFieldBasicInfo(caption = "Holiday Division Short Name", type = "varchar(100)")
+	private String shortName;
+	
+	@Override
+	public String getId() {
+		return divisionId;
+	}
+
+	@Override
+	public void setId(String itemId) {
+		divisionId = itemId;
+	}
+
+	/* (non-Javadoc)
+	 * @see paperworker.core.PWItem#getValue(java.lang.String)
+	 */
+	@Override
+	public Object getValue(String fieldName) throws PWError {
+		return PWItem.getValue(this, fieldName);
+	}
+
+	/* (non-Javadoc)
+	 * @see paperworker.core.PWItem#setValue(java.lang.String, java.lang.Object)
+	 */
+	@Override
+	public void setValue(String fieldName, Object value) throws PWError {
+		PWItem.setValue(this, fieldName, value);
+	}
+}
---
+++ paperworker.holidaydivision.ui.command/src/paperworker/holidaydivision/ui/command/ListAction.java
@@ -0,0 +1,54 @@
+/*
+ *  $Id: ListAction.java 2013/09/21 3:03:36 Masamitsu Oikawa $
+ *
+ *  ===============================================================================
+ *
+ *   Copyright (C) 2013  Masamitsu Oikawa  <oicawa@gmail.com>
+ *   
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *   
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ *
+ *  ===============================================================================
+ */
+
+package paperworker.holidaydivision.ui.command;
+
+import paperworker.core.ui.command.PaperWorker;
+import paperworker.holidaydivision.core.HolidayDivision;
+import paperworker.holidaydivision.core.HolidayDivisionController;
+import paperworker.master.ui.command.MasterListAction;
+
+public class ListAction extends MasterListAction<HolidayDivision, HolidayDivisionController> {
+
+	public ListAction() {
+		super();
+	}
+
+	@Override
+	protected void printItem(HolidayDivision item) {
+		PaperWorker.message("%s : %s (%s)",
+				item.getDivisionId(),
+				item.getName(),
+				item.getShortName());
+	}
+
+	@Override
+	protected Class<HolidayDivision> getItemType() {
+		return HolidayDivision.class;
+	}
+}
